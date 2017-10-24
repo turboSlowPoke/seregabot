862891229c464a08b3ee8b1317666965d33a5e82
turboSlowPoke <mega_pokemon@mail.ru>
2017-10-24 17:12:44 +0300

    1

---
+++ src/main/java/dbservices/entyties/Currency.java
@@ -0,0 +1,11 @@
+package dbservices.entyties;
+
+import java.time.LocalDateTime;
+
+public class Currency {
+    private long id;
+    private LocalDateTime addTime;
+    private String miningIsAvail;
+    private String name;
+    private String description;
+}
